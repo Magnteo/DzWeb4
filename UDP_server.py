dbc23f411257f2a5ceebe9bbadacfba5cebc20a6
Magnteo <bogdanserg48@gmail.com>
2025-08-13 10:28:25 +0300

    100

--- UDP_server.py
+++ UDP_server.py
@@ -1,30 +1,33 @@
-import socket , json 
+import socket, json
 from pathlib import Path
 from datetime import datetime
-UDP_soket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-UDP_soket.bind(("127.0.0.1",5000))
+
+UDP_soket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UDP_soket.bind(("127.0.0.1", 5000))
 file_path = Path("storage/data.json")
+
+
 def func_UDP_soket():
     while True:
-        data , addr = UDP_soket.recvfrom(4096)
-        
+        data, addr = UDP_soket.recvfrom(4096)
+
         try:
-            decoded = data.decode('utf-8')
+            decoded = data.decode("utf-8")
 
             parsed_dict = json.loads(decoded)
 
             if not file_path.exists():
-                data_all ={}
+                data_all = {}
             else:
-                with open(file_path,'r',encoding='utf-8') as file:
-                    data_all =json.load(file)
-                    
-            time=datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+                with open(file_path, "r", encoding="utf-8") as file:
+                    data_all = json.load(file)
+
+            time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
             data_all[time] = parsed_dict
 
-            with open(file_path ,"w",encoding="utf-8") as file:
-                json.dump(data_all,file,ensure_ascii=False,indent=4)
-                
-        except (json.JSONDecodeError , UnicodeDecodeError):
-            print('ERROR')
+            with open(file_path, "w", encoding="utf-8") as file:
+                json.dump(data_all, file, ensure_ascii=False, indent=4)
+
+        except (json.JSONDecodeError, UnicodeDecodeError):
+            print("ERROR")
             continue
--- main.py
+++ main.py
@@ -1,46 +1,48 @@
-from http.server import BaseHTTPRequestHandler , HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from pathlib import Path
 import json
 import socket
 from urllib.parse import parse_qs
 from UDP_server import func_UDP_soket
 from threading import Thread
+
+
 class MyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        if self.path == '/message':
-            content_length =int(self.headers.get('Content-Length',0))
+        if self.path == "/message":
+            content_length = int(self.headers.get("Content-Length", 0))
             post_data = self.rfile.read(content_length)
-            decoded_data = post_data.decode('utf-8')
-            
-            parsed = parse_qs(decoded_data) 
-            parsed_dict ={
-                "username": parsed.get("username",[""])[0],
-                "message": parsed.get("message",[""])[0],
+            decoded_data = post_data.decode("utf-8")
+
+            parsed = parse_qs(decoded_data)
+            parsed_dict = {
+                "username": parsed.get("username", [""])[0],
+                "message": parsed.get("message", [""])[0],
             }
 
-            sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             json_string = json.dumps(parsed_dict)
 
-            sock.sendto(json_string.encode('utf-8'),("localhost",5000))
+            sock.sendto(json_string.encode("utf-8"), ("localhost", 5000))
             sock.close()
-            
+
             self.send_response(302)
-            self.send_header("Location",'/message')
+            self.send_header("Location", "/message")
             self.end_headers()
 
     def do_GET(self):
-        if self.path.startswith('/static'):
+        if self.path.startswith("/static"):
             file_path = Path(self.path.lstrip("/"))
             if file_path.exists():
-                if file_path.suffix ==".css":
-                    content_type = 'text/css'
-                elif file_path.suffix ==".png":
+                if file_path.suffix == ".css":
+                    content_type = "text/css"
+                elif file_path.suffix == ".png":
                     content_type = "image/png"
                 else:
-                    content_type ="application/octet-stream"
+                    content_type = "application/octet-stream"
 
                 self.send_response(200)
-                self.send_header("Content-type",content_type)
+                self.send_header("Content-type", content_type)
                 self.end_headers()
                 self.wfile.write(file_path.read_bytes())
                 return
@@ -49,18 +51,18 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write("Файл не знайдено".encode("utf-8"))
                 return
-        if self.path =='/':
+        if self.path == "/":
             file_path = Path("index.html")
             code = 200
-        elif self.path == '/message':
+        elif self.path == "/message":
             file_path = Path("message.html")
-            code=200
+            code = 200
         else:
             file_path = Path("error.html")
             code = 404
         if file_path.exists():
             content = file_path.read_text(encoding="utf-8")
-            self.send_response(code) 
+            self.send_response(code)
             self.send_header("Content-type", "text/html; charset=utf-8")
             self.end_headers()
             self.wfile.write(content.encode("utf-8"))
@@ -70,7 +72,8 @@ class MyHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write("index.html не знайдено".encode("utf-8"))
 
-udp_thread = Thread(target=func_UDP_soket,daemon= True)
+
+udp_thread = Thread(target=func_UDP_soket, daemon=True)
 udp_thread.start()
 
 server = HTTPServer(("localhost", 3000), MyHandler)
